b718c8652cc0985fa9fea54688b1a105559c41e7
James <wong.james2003@gmail.com>
2023-02-06 14:11:42 -0500

    found all solutions

--- assigns/02/MySolution/assign02_02.py
+++ assigns/02/MySolution/assign02_02.py
@@ -2,6 +2,7 @@
 import sys
 sys.path.append('..')
 from assign02 import *
+from assign02_01 import *
 ####################################################
 print("[import ./../assign02.py] is done!")
 ####################################################
@@ -10,3 +11,24 @@ print("[import ./../assign02.py] is done!")
 # mylist_quicksort (see list_quicksort in assign02.sml)
 #
 ####################################################
+
+def mylist_quicksort(xs):
+    def qsort(xs):
+        if mylist_nilq(xs):
+            return mylist_nil()
+        else:
+            (ys, zs) = qpart(xs.cons2, xs.cons1)
+            ys = qsort(ys)
+            zs = qsort(zs)
+            return mylist_append(ys, mylist_append(mylist_cons(xs.cons1, mylist_nil()), zs))
+    def qpart(xs, p0):
+        if mylist_nilq(xs):
+            return (mylist_nil(), mylist_nil())
+        else:
+            (ys, zs) = qpart(xs.cons2, p0)
+            if xs.cons1 <= p0:
+                return (mylist_cons(xs.cons1, ys), zs)
+            else:
+                return (ys, mylist_cons(xs.cons1, zs))
+    
+    return qsort(xs)
--- assigns/02/MySolution/assign02_03.py
+++ assigns/02/MySolution/assign02_03.py
@@ -10,3 +10,47 @@ print("[import ./../assign02.py] is done!")
 # mylist_mergesort (see list_mergesort in assign02.sml)
 #
 ####################################################
+
+def mylist_mergesort(xs):
+    def split(xs):
+        if mylist_nilq(xs):
+            return (mylist_nil(), mylist_nil())
+        else:
+            x1 = xs.cons1
+            xs = xs.cons2
+            if mylist_nilq(xs):
+                return (mylist_cons(x1, mylist_nil()), mylist_nil())
+            else:
+                x2 = xs.cons1
+                xs = xs.cons2
+                (ys, zs) = split(xs)
+                return (mylist_cons(x1, ys), mylist_cons(x2, zs))
+            
+    def merge(ys, zs):
+        if mylist_nilq(ys):
+            return zs
+        else:
+            y1 = ys.cons1
+            ys = ys.cons2
+            if mylist_nilq(zs):
+                return mylist_cons(y1, ys)
+            else:
+                z1 = zs.cons1
+                zs = zs.cons2
+                if y1 <= z1:
+                    return mylist_cons(y1, merge(ys, mylist_cons(z1, zs)))
+                else:
+                    return mylist_cons(z1, merge(mylist_cons(y1, ys), zs))
+                
+    if mylist_nilq(xs):
+        return mylist_nil()
+    else:
+        x1 = xs.cons1
+        xs = xs.cons2
+        if mylist_nilq(xs):
+            return mylist_cons(x1, mylist_nil())
+        else:
+            x2 = xs.cons1
+            xs = xs.cons2
+            (ys, zs) = split(xs)
+            return merge(mylist_mergesort(mylist_cons(x1, ys)), mylist_mergesort(mylist_cons(x2, zs)))
