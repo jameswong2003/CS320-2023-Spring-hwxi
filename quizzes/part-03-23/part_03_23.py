74c510deb831d223057baeea6dbee30ad5813f4a
James <wong.james2003@gmail.com>
2023-03-23 16:50:36 -0400

    changes

--- quizzes/part-03-23/part_03_23.py
+++ quizzes/part-03-23/part_03_23.py
@@ -58,9 +58,9 @@ def image_copy(image):
 def image_rotate_090r(image):
     ww = image.width
     hh = image.height
-    i1 = imgvec.image_make_pylist(ww, hh, int1_map_pylist(hh*ww , lambda ij: imgvec.image_get_pixel(image, ij%hh, ij//hh)))
-    i2 = imgvec.image_make_pylist(hh, ww, int1_map_pylist(hh*ww , lambda ij: imgvec.image_get_pixel(i1, ij%ww, ij//ww)))
-    return imgvec.image_make_pylist(ww, hh, int1_map_pylist(hh*ww , lambda ij: imgvec.image_get_pixel(i2, ij%hh, ij//hh)))
+    return \
+        imgvec.image_make_pylist\
+        (ww, hh, int1_map_pylist(hh*ww, lambda ij: imgvec.image_get_pixel(image, (ij-ww)%hh , (ij-hh)//ww )))
 ###################################################
 balloons = \
     load_color_image\
