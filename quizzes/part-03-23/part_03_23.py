2f771036232788da6a6545828a8a15ff064c8e3f
James <wong.james2003@gmail.com>
2023-03-28 16:49:53 -0400

    completed quiz

--- quizzes/part-03-23/part_03_23.py
+++ quizzes/part-03-23/part_03_23.py
@@ -60,7 +60,7 @@ def image_rotate_090r(image):
     hh = image.height
     return \
         imgvec.image_make_pylist\
-        (ww, hh, int1_map_pylist(hh*ww, lambda ij: imgvec.image_get_pixel(image, -ij%hh , -ij//hh )))
+        (ww, hh, int1_map_pylist(hh*ww, lambda ij: imgvec.image_get_pixel(image, hh-1-ij%hh, ij//hh )))
 ###################################################
 balloons = \
     load_color_image\
